72a7595116e5101eee61a85ecda8b6e932d9085c
Ivan Vorobei <hello@ivanvorobei.io>
2024-08-26 16:18:14 +0300

    Update NSDirectionalEdgeInsetsExtension.swift

--- Sources/SwiftBoost/UIKit/Extensions/NSDirectionalEdgeInsetsExtension.swift
+++ Sources/SwiftBoost/UIKit/Extensions/NSDirectionalEdgeInsetsExtension.swift
@@ -5,4 +5,8 @@ public extension NSDirectionalEdgeInsets {
     init(_ insets: UIEdgeInsets) {
         self.init(top: insets.top, leading: insets.left, bottom: insets.bottom, trailing: insets.right)
     }
+    
+    var edgeInstset: UIEdgeInsets {
+        return .init(top: top, left: leading, bottom: bottom, right: trailing)
+    }
 }
